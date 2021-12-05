5486b6d37c4f043df187eca6868362494d72b741
Tangui Hoarau <83754212+Sana83@users.noreply.github.com>
2021-12-05 17:49:28 +0100

    Partie PDF

--- tests/pdfTest/genpdf.php
+++ tests/pdfTest/genpdf.php
@@ -1,12 +1,12 @@
-<?php
-use Dompdf\Dompdf;
-
-require_once 'includes/dompdf/autoload.inc.php';
-
-$dompdf = new Dompdf();
-
-$dompdf->loadHtml('Brouette');
-
-$dompdf->render();
-
-//$dompdf->stream();
+<?php //
+//use Dompdf\Dompdf;
+//
+//require_once 'includes/dompdf/autoload.inc.php';
+//
+//$dompdf = new Dompdf();
+//
+//$dompdf->loadHtml('Brouette');
+//
+//$dompdf->render();
+//
+////$dompdf->stream();
